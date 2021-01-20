343e36e9c4671bd15707c027709ec02d555ceb0f
big-jiang <709256076@qq.com>
2021-01-20 12:52:42 +0800

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+let Koa = require('koa');
+let Router = require('koa-router');
+let bodyparser = require('koa-bodyparser');
+let axios = require('axios');
+let router = new Router()
+let app = new Koa();
+let url = 'http://172.24.242.182:8080';
+let data = {idcard:'321321199710057015'}
+
+app.use(bodyparser());
+app.use(async (ctx,next)=>{
+	let stime = new Date().getTime();
+	await next();
+	let etime = new Date().getTime();
+	console.log('time:'+(etime-stime)+'ms')
+})
+console.log('101')
+router.post('/getHealth',async(ctx)=>{ 
+    let {name,idcard} = ctx.request.body;
+    console.log(`name:${name}-${idcard}`);
+       //let url = 'http://172.24.242.182:8080/getHealth?idcard='+idcard;
+       //let data = {idcard:idcard};
+       //await axios.post(url,data).then(res=>{
+	//ctx.body = {code:200,data:res.data}}).catch(err=>{
+	//ctx.body = {code:401,data:err}});
+	let url = 'http://172.24.242.182:8080';
+	
+	let result = await axios.post(url,{idcard:idcard})
+	console.log(result.data)
+	try{
+	ctx.body = result.data;
+	}catch(err){cosole.err}
+});
+async function getHealth(){
+	return await axios.get(url,data);
+}
+app.use(router.routes());
+app.listen(8080);
+=======
+console.log(101)
+>>>>>>> b314027c6d4a008b7d97ae5552efb59b35f2e3a0
