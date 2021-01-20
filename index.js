e8c82c141d19bfb750de5825f92c8bc729c0eb38
big-jiang <52397707+big-jiang@users.noreply.github.com>
2021-01-20 13:36:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ router.post('/getHealth',async(ctx)=>{
     let {name,idcard} = ctx.request.body;
     console.log(`name:${name}-${idcard}`);
        //let url = 'http://172.24.242.182:8080/getHealth?idcard='+idcard;
-       //let data = {idcard:idcard};
        //await axios.post(url,data).then(res=>{
 	//ctx.body = {code:200,data:res.data}}).catch(err=>{
 	//ctx.body = {code:401,data:err}});
